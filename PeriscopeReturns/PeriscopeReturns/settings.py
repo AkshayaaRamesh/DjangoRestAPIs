17ea846a6e65ff428918a4ed33b002b25676418c
Akshayaa Ramesh <63770776+AkshayaaRamesh@users.noreply.github.com>
2020-07-30 10:24:46 +0530

    Update settings.py

--- PeriscopeReturns/PeriscopeReturns/settings.py
+++ PeriscopeReturns/PeriscopeReturns/settings.py
@@ -81,10 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': '',
-        'USER': 'postgres',
+        'USER': '',
         'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
